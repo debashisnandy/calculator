df44175dba47318a2e5e3d13a8132fe21a0e622c
ronirn <31581078+ronirn@users.noreply.github.com>
2018-06-26 13:29:24 +0530

    Update MyCalUi.py

--- GUI_Home/MyCalUi.py
+++ GUI_Home/MyCalUi.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'calculator.ui'
-#
-# Created by: PyQt5 UI code generator 5.10.1
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QRegExp
